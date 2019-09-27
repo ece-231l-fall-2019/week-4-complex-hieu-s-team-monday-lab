3caabc3cbad8a8b006eb317723aa8139c9e04d86
hquang1999 <hquang@unm.edu>
2019-09-26 19:15:29 -0600

    Updated Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,4 +1,5 @@
 #include "Complex.h"
+#include <math.h>
 
 Complex::Complex()
 {
@@ -6,4 +7,45 @@ Complex::Complex()
 	_imag = 0.0;
 }
 
+//Constructors
+Complex::Complex(double re, double im) 
+{
+	_real = re;
+	_imag = im;
+}
+
+Complex::Complex(const Complex& z) 
+{
+	_real = z.real();
+	_imag = z.imag();
+}
+
+//Access Values
+double Complex::real() const {return _real;}
+double Complex::imag() const {return _imag;}
+
+//Assignment
+Complex& Complex::operator=(double r)
+{
+	_real = r;
+	_imag = r;
+
+	return *this;
+}
+
+Complex& Complex::operator=(const Complex& z)
+{
+	_real = z._real;
+	_imag = z._imag;
+
+	return *this;
+}
+
+Complex& Complex::operator+=(const Complex& z)
+{
+	_real += z._real;
+	_imag += z._imag;
+	
+	return *this;
+}
 
