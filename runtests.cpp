332f599dff7e0119ed1c6532802fd1cc04071681
Bryan Carrillo <apaperairplane@unm.edu>
2019-09-27 11:10:36 -0600

    update 2

--- runtests.cpp
+++ runtests.cpp
@@ -30,26 +30,25 @@ int main()
 	// TODO:
 	// Write *at least* 20 more tests to fully test
 	// your Complex class.
-	Complex z2(4,3);
-	Complex a1(7,3);
-	Complex a2(3,0);
-
+	
+	Assert(z1 == Complex(4,3), "Comparison == (complex)");
 	Assert(z1 == z1, "Comparison == (complex)");
-	Assert(z1 == z2, "Comparison == (complex)");
-	Assert(z1 != a1, "Comparison != (complex)");
+	Assert(z1 != (5,7), "Comparison != (complex)");
 	Assert(z1 != 3, "Comparison != (double)");
-	Assert(a2 == 3, "Comparison == (double)");
+	z1 = Complex(3,0);
+	Assert(z1 == 3, "Comparison == (double)");
 
 	z1 = 5;
 	Assert(z1 == 5, "Operator = (double)");
 	z1 = Complex(4,3);
-	Assert(z1 == z2, "Operaror = (complex)");
-	z1 += z2;
-	z2 = Complex(8,6);
-	Assert(z1 == z2, "Operator +=");
+	Assert(z1 == Complex(4,3), "Operator = (complex)");
+	z1 += Complex(4,3);
+	Assert(z1 == Complex(8,6), "Operator +=");
 	z1 -= Complex(4,3);
 	Assert(z1 == Complex(4,3), "Operator -=");
-	
+	z1 *= Complex(4,3);
+	Assert(z1 == Complex(16,9), "Operator *=");
+
 
 	std::cout << norm(z1) << " : Expected Value: 2.646 " << std::endl;
 	
